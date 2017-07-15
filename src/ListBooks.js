408831d0969272b306aea096682a0e4c417340e6
Dejan <dejanhadziomerovic@gmail.com>
2017-07-15 21:27:00 +0200

    Add ListBooks and SearchBooks components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class ListBooks extends React.Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired
+    }
     render() {
         return (
-            <div>List Books</div>
+            <div>
+                <ul>
+                    {this.props.books.map((book) => (
+                        <li key={book.id}>{book.title}</li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
