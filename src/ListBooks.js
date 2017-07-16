52d47cdfabfcfb058a3041009e71420ab7359d35
Dejan <dejanhadziomerovic@gmail.com>
2017-07-16 15:01:34 +0200

    Add BookShelf and Book components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf'
+// import PropTypes from 'prop-types';
 
 class ListBooks extends React.Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired
+    // static propTypes = {
+    //     books: PropTypes.array.isRequired
+    // }
+    state = {
+        books: []
+    }
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books });
+        });
     }
     render() {
         return (
             <div>
-                <ul>
-                    {this.props.books.map((book) => (
-                        <li key={book.id}>{book.title}</li>
-                    ))}
-                </ul>
+                <BookShelf title="Currently Reading" books={this.state.books.filter(book => {
+                    return book.shelf === 'currentlyReading'
+                })} />
+                <BookShelf title="Want To Read" books={this.state.books.filter(book => {
+                    return book.shelf === 'wantToRead'
+                })} />
+                <BookShelf title="Read" books={this.state.books.filter(book => {
+                    return book.shelf === 'read'
+                })} />
             </div>
         )
     }
